8641c598541e083bf94e62fece9a819f58f5a90c
remote-torrent-6747 <abbyc@sky.com>
2021-04-09 16:37:13 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,20 @@
 class Dog
   attr_accessor :name, :breed, :age
 
+  @@all = []
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
   end
 
-  def self.save 
+  def self.save
+    @@all << self
   end
 
-  def all
+  def self.all
+    @@all
   end
 
 end
